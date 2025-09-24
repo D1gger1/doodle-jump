e4f12d347122a75f460b01ae6d03bcfd31c5b2ab
Max <conr9811@gmail.com>
2025-09-24 12:24:38 +0200

    Add border and doodler code in js

---
+++ script.js
@@ -0,0 +1,20 @@
+// board 
+let board;
+let boardWidth = 360;
+let boardHeight = 576;
+let context;
+
+//doodler
+let doodlerWidth = 46;
+let doodlerHeight = 46;
+let doodlerX = boardWidth / 2 - doodlerWidth / 2;
+let doodlerY = doodlerHeight * 7 / 8 - doodlerHeight;
+let doodlerRightImg;
+let doodlerLeftImg;
+
+let doodler = {
+    img: null,
+    x: doodlerX,
+    y: doodlerY,
+    width:
+}
